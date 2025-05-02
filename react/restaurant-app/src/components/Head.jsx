d63ebaa7dd6b7128c32da8b61473f897410bc76c
akverma2496 <akverma2496@gmail.com>
2025-05-02 14:54:14 +0530

    summary-added

--- react/restaurant-app/src/components/Head.jsx
+++ react/restaurant-app/src/components/Head.jsx
@@ -9,6 +9,11 @@ const Head = () => {
         <button className={styles.button}>Your Cart {0}</button>
     </div>
     <img className={styles.image} src="../src/assets/meals.jpg" alt="meal" />
+    <div className={styles.float}>
+      <h1>Delicious Food, Delivered To you</h1><br />
+      <p>Choose your favorite meal from our broad selection of available meals and enjoy a delicious lunch or dinner at home.</p><br />
+      <p>All meals are cooked with high quality ingredients, just-in-time and of course by experienced chefs!</p>
+    </div>
     </>
   )
 }
